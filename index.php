99892d622be08401a5b12f611e98067ded303176
David Alienyi <alienyidavid4christ@gmail.com>
2021-05-28 01:44:05 +0100

    fix

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
   session_start();
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
   include 'controller.php';
   $subPath = $_SERVER['HTTP_HOST'] == 'localhost:8888' ? '/zenith-dashboard' : '';
   $currentPage = 'Dashboard';
